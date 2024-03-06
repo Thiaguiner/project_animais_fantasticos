d8d455a13bd513908efd0cdee30738b979fd37d1
Thiaguiner <tcarneiro990@gmail.com>
2024-03-06 15:31:12 -0300

    colocando animação

--- script.js
+++ script.js
@@ -1,22 +1,89 @@
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo"); //para deixar o primeiro item da pag ativo, para nao ficar em branco
 
-if(tabMenu.length && tabContent.length){
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo"); //adicionando uma classe para quando eu apertar na foto do animal, vai para a descrição dele
+    }
 
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index); //ativando a função do que falei acima
+      });
+    });
+  }
+}
+initTabNav();
 
-tabContent[0].classList.add('ativo')//para deixar o primeiro item da pag ativo, para nao ficar em branco
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
 
-function activeTab(index) {
-  tabContent.forEach((section) => {
-    section.classList.remove('ativo')
+  if (accordionList.length) {
+    accordionList[0].classList.add("ativo");
+    accordionList[0].nextElementSibling.classList.add("ativo");
+
+    function activeAccordion() {
+      this.classList.toggle("ativo");
+      this.nextElementSibling.classList.toggle("ativo");
+    }
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    // section.scrollIntoView({
+    //   behavior: 'smooth',
+    //   block: 'start',
+    // });
+
+    // forma alternativa
+    const topo = section.offsetTop;
+    window.scrollTo({
+      top: topo,
+      behavior: "smooth",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
   });
-  tabContent[index].classList.add("ativo");//adicionando uma classe para quando eu apertar na foto do animal, vai para a descrição dele
 }
+initScrollSuave();
 
-tabMenu.forEach((itemMenu, index)=> {
-  itemMenu.addEventListener('click', () => {
-    activeTab(index);//ativando a função do que falei acima
-  })
-})
+function initAnimacaoScroll(){
+
+
+const sections = document.querySelectorAll(".js-scroll");
+if(sections.length){
+
+
+const windowMetade = window.innerHeight * 0.6;
+
+function animaScroll() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top - windowMetade;
+    if (sectionTop < 0) {
+      section.classList.add("ativo");
+    }
+  });
 }
+animaScroll();
+window.addEventListener("scroll", animaScroll);
+}
+}
+initAnimacaoScroll();
